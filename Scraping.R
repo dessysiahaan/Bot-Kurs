1ad2db93a5396bfeb46d19ebc1f7d2002ede7b27
dessysiahaan <61528426+dessysiahaan@users.noreply.github.com>
2022-06-23 13:48:34 +0700

    Add files via upload

---
+++ Scraping.R
@@ -0,0 +1,37 @@
+library(rvest)
+library(stringr)
+
+#Scraping
+  
+
+url <- "https://www.bi.go.id/en/statistik/informasi-kurs/jisdor/Default.aspx"
+html <- url %>% read_html
+
+
+
+jisdor <- html %>%
+  html_node(".table-lg") %>% #table-lg
+  html_table
+
+str(jisdor)
+jisdor
+
+
+
+today <- format(Sys.time(), "%d")
+jisdor$Day <- unlist(lapply(str_split(jisdor$Date, " "), `[[`, 1))
+newdate <- jisdor[which(jisdor$Day==today),]
+
+
+#Connect to MongoDB
+  
+
+connection_string = 'mongodb+srv://dbmds001:sta562mds@STAMDS.kuzgp.mongodb.net/?retryWrites=true&w=majority'
+kurs_bi = mongo(collection="kurs_jisdor",
+                db="bot_kurs",
+                url=connection_string)
+
+
+#Insert Data to MongoDB
+
+kurs_bi$insert(newdate)
